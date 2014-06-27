6e6460b1e734623bcea855ff55db00764457902c
Yosmany Garcia <yosmanyga@gmail.com>
2014-06-27 18:35:20 -0400

    Added bootstrap test files. Added a compiler pass for doctrine mapping

---
+++ DependencyInjection/Compiler/DoctrineMappingPass.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Cubalider\Bundle\MoneyBundle\DependencyInjection\Compiler;
+
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Compiler\CompilerPassInterface;
+use Symfony\Component\DependencyInjection\Definition;
+use Symfony\Component\DependencyInjection\Reference;
+
+class DoctrineMappingPass implements CompilerPassInterface
+{
+    /**
+     * {@inheritDoc}
+     */
+    public function process(ContainerBuilder $container)
+    {
+        $definition = new Definition('Doctrine\ORM\Mapping\Driver\XmlDriver', array(array(
+            sprintf("%s/../../../../../../money-with-doctrine-orm/src/Cubalider/Component/Money/Resources/config/doctrine", __DIR__)
+        )));
+        $definition->setPublic(false);
+        $container->setDefinition('cubalider_money.xml_driver', $definition);
+
+        $definition = $container->getDefinition('doctrine.orm.default_metadata_driver');
+        $definition->addMethodCall('addDriver', array(new Reference('cubalider_money.xml_driver'), 'Cubalider\Component\Money\Model'));
+    }
+}
